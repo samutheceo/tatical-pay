8c6e7117bd60bcc7639146ee1afa8f90bdd35f9a
Samu The Ceo <samutheceo@gmail.com>
2024-06-10 03:16:24 +0200

    Fx

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 
 const Home: React.FC = () => {
   const [bannerIndex, setBannerIndex] = useState(0);
+  const [showCryptoAddress, setShowCryptoAddress] = useState(false);
   const banners = [
     "/banner1.gif",
     "/banner2.gif",
@@ -27,25 +28,31 @@ const Home: React.FC = () => {
     window.open("https://paypal.me/samutheceo?country.x=IT&locale.x=it_IT", "_blank");
   };
 
+  const handleCryptClick = () => {
+    setShowCryptoAddress(true);
+  };
+
   return (
     <div className="absolute left-0 top-0 w-screen h-[100%] overflow-hidden select-none">
       <div className="w-full h-full z-0 absolute">
-      <Image
-        src={banners[bannerIndex]}
-        alt=""
-        layout="fill"
-        className="w-screen h-screen object-cover"
-      />
+        <Image
+          src={banners[bannerIndex]}
+          alt=""
+          layout="fill"
+          className="w-screen h-screen object-cover"
+        />
       </div>
       <div className="w-full h-full z-10 bg-black/80 backdrop-blur-sm flex flex-col items-center justify-center">
         <Squircle cornerRadius={25} cornerSmoothing={0.6} asChild className='w-fit h-fit px-8 py-4 bg-white/30 backdrop-blur-sm flex flex-col items-center justify-center md:scale-[1] scale-[0.9]'>
           <section className='w-fit h-fit flex flex-col items-center justify-center text-center gap-y-2'>
             <div className='rounded-full w-24 h-24 overflow-hidden flex items-center justify-center'>
-            <Image
-              src="/samu.gif"
-              alt=""
-              className='relative rounded-full w-full h-full'
-            />
+              <Image
+                src="/samu.gif"
+                alt=""
+                width={24}
+                height={24}
+                className='relative rounded-full w-full h-full'
+              />
             </div>
             <div className='flex flex-col w-fit h-fit text-center items-center justify-center'>
               <h1 className='font-semibold text-white text-xl'>&quot;samu</h1>
@@ -69,12 +76,18 @@ const Home: React.FC = () => {
                   <span>PayPal</span>
                 </span>
               </Squircle>
-              <Squircle onClick={handlePaypalClick} cornerRadius={13} cornerSmoothing={0.6} asChild className='w-72 h-fit bg-orange-500 text-white text-sm px-4 py-2 flex items-center justify-center cursor-pointer transition-all duration-200 ease-linear hover:opacity-60'>
-                <span className='flex flex-wrap gap-x-2'>
-                  <span className='text-xl'><BiLogoBitcoin /></span>
-                  <span>Crypto</span>
-                </span>
-              </Squircle>
+              {showCryptoAddress ? (
+                <div className='text-[0.7rem] flex text-center items-center justify-center font-mono text-zinc-500 bg-zinc-800 rounded-lg w-72 h-fit px-4 py-2 select-text'>
+                  0x590d869457390dcf0b8dbf7ef4f5810d4be4d3b8
+                </div>
+              ) : (
+                <Squircle onClick={handleCryptClick} cornerRadius={13} cornerSmoothing={0.6} asChild className='w-72 h-fit bg-orange-500 text-white text-sm px-4 py-2 flex items-center justify-center cursor-pointer transition-all duration-200 ease-linear hover:opacity-60'>
+                  <span className='flex flex-wrap gap-x-2'>
+                    <span className='text-xl'><BiLogoBitcoin /></span>
+                    <span>Crypto</span>
+                  </span>
+                </Squircle>
+              )}
             </div>  
           </section>
         </Squircle>
